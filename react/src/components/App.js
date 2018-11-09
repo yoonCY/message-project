e6e630774706c93c727d9e4082b52757723545ec
yooncy <32188482+yoonCY@users.noreply.github.com>
2018-11-09 19:23:57 +0900

    Add files via upload

---
+++ react/src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+class App extends React.Component {
+    render(){
+
+        return (
+            <div>
+                <h1>Hello React Skeleton</h1>
+                <div>
+                    test   
+                </div>
+            </div>
+                
+        );
+    }
+}
+
+export default App;
